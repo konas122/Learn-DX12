26c1ace32403abc5e720be7e79ec2b35dcf863b0
konas122 <2407626470@qq.com>
2024-11-24 15:35:35 +0800

    initial commit

---
+++ Common/FrameResource.cpp
@@ -0,0 +1,50 @@
+#include "FrameResource.h"
+
+
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount)
+{
+	ThrowIfFailed(device->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(CmdListAlloc.GetAddressOf())
+	));
+
+	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+	ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+}
+
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT vertCount, InitializeType flag)
+{
+	ThrowIfFailed(device->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(CmdListAlloc.GetAddressOf())
+	));
+
+	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+	ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+
+	if (flag == InitializeType::material)
+	{
+		MaterialCB = std::make_unique<UploadBuffer<MaterialConstants>>(device, vertCount, true);
+	}
+	else
+	{
+		WavesVB = std::make_unique<UploadBuffer<Vertex>>(device, vertCount, false);
+	}
+}
+
+FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount, UINT materialCount, UINT waveVertCount)
+{
+	ThrowIfFailed(device->CreateCommandAllocator(
+		D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(CmdListAlloc.GetAddressOf())
+	));
+
+	PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+	ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+	MaterialCB = std::make_unique<UploadBuffer<MaterialConstants>>(device, materialCount, true);
+
+	WavesVB = std::make_unique<UploadBuffer<Vertex>>(device, waveVertCount, false);
+}
+
+
+FrameResource::~FrameResource() {}
